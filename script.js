2f88c4a90c865260f6025fc29e90b8283a3b6f31
Ridhwan Mazlan <Treedots@users.noreply.github.com>
2022-08-25 02:43:11 +0000

    Added Image

--- script.js
+++ script.js
@@ -11,5 +11,5 @@ $(document).ready(function() {
   const dd = new Date();
   const yy = dd.getFullYear() - 2009;
   $(".target-name").text(`Hi there, I'm Ridhwan. Nice to meet you.`)
-  $(".target").text(`My journey as a programmer started ${yy} year ago. Various programming languages i have dabble and learned during my Journey. From C, C++, C#, Java, Javascript, and Python. I'm quiet most of the time but enthusiastic when talking things i love. Currently looking out to pursue my career path as Software Engineer/Developer and move away from this current Industry.`);
+  $(".target").text(`My journey as a programmer started ${yy} year ago. Various programming languages i have dabble and learned during my Journey. From C, C++, C#, Java, Javascript, and Python. I'm quiet most of the time but enthusiastic when talking things i love. Currently looking out to pursue and focus career path as Software Engineer/Developer.`);
 });
